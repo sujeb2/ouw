9e6e0db63338574c2615e6b87e545b13bf457c79
tempu <sdbb2q@gmail.com>
2025-10-10 16:24:48 +0900

    Initial commit

---
+++ src/main/java/com/songro/ouw/Ouw.java
@@ -0,0 +1,17 @@
+package com.songro.ouw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ouw extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
